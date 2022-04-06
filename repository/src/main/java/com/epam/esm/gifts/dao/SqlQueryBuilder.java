df5e94a373a46481a5b9c7ee78b2007bb3e7e316
Yaraslau <gamasileusbed@gmail.com>
2022-04-06 21:57:14 +0300

    add sql queryBuilder for update

---
+++ repository/src/main/java/com/epam/esm/gifts/dao/SqlQueryBuilder.java
@@ -0,0 +1,42 @@
+package com.epam.esm.gifts.dao;
+
+import java.util.List;
+import java.util.Objects;
+
+import static com.epam.esm.gifts.dao.constants.SqlQuery.SEARCH_AND_SORT_QUERY;
+
+public class SqlQueryBuilder {
+
+    private static final String DEFAULT_SORT = "ASC";
+    private static final String GIFT_ID = "gift_certificate.id";
+    private static final String EMPTY = "";
+    private static final String SPACE = " ";
+    private static final String COMMA_SIGN = ", ";
+
+
+    public static String buildCertificateQueryForSearchAndSort(String tagName, String searchPart,
+                                                               String description, List<String> sortingFieldList,
+                                                               String orderSort) {
+        tagName = tagName != null ? tagName : EMPTY;
+        searchPart = searchPart != null ? searchPart : EMPTY;
+
+        String queryMainPart = String.format(SEARCH_AND_SORT_QUERY, tagName, searchPart, description);
+        StringBuilder resultQuery = new StringBuilder(queryMainPart);
+
+        if (sortingFieldList != null && !sortingFieldList.isEmpty()) {
+            sortingFieldList.forEach(field -> resultQuery
+                    .append(GIFT_ID)
+                    .append(field)
+                    .append(SPACE)
+                    .append(Objects.requireNonNullElse(orderSort, DEFAULT_SORT))
+                    .append(COMMA_SIGN));
+            System.out.println(resultQuery);
+            resultQuery.deleteCharAt(resultQuery.length() - 2);
+            System.out.println(resultQuery);
+        } else {
+            resultQuery.append(GIFT_ID).append(SPACE).append(Objects.requireNonNullElse(orderSort, DEFAULT_SORT));
+            System.out.println(resultQuery);
+        }
+        return resultQuery.toString();
+    }
+}
